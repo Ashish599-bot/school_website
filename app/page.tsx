db6fb775a51a0a8ba896e756721d2e067b833d76
Ashish <ashishsky619gmail.com>
2025-11-03 02:38:58 -0700

    adding navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,34 @@
-export function Home() {}
+import { Navbar } from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="h-[300vh]">
+      <nav className="flex justify-between items-center mx-auto py-6 px-4 bg-blue-900 w-full fixed top-0">
+        <div className="text-2xl font-bold text-white ">T3 Academy</div>
+        <ul className="hidden md:flex space-x-8 text-gray-600 font-medium">
+          <li>
+            <a className="hover:text-gray-400 cursor-pointer shadow-lg text-white">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a className="hover:text-gray-400 cursor-pointer shadow-lg text-white">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-gray-400 cursor-pointer shadow-lg text-white">
+              Contact Us
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-gray-400 cursor-pointer shadow-lg text-white">
+              Alumni
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
