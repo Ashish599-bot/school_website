43cd7a6cfec1f57d4db06a7bdbd5c1cf948e62fc
taaro <demal2525@gmail.com>
2025-11-03 15:03:55 +0600

    second

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-zinc-50 font-sans p-10 ">
+    <main className="flex min-h-screen bg-zinc-50 font-sans p-10 ">
       <h1 className="text-black text-4xl">T3 Academy</h1>
-
-    </div>
+    </main>
   );
 }
