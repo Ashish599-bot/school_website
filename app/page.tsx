4238d5cea1c5f6f36d55034804bb6185391d09c5
taaro <demal2525@gmail.com>
2025-11-04 10:31:51 +0600

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
-
-
+'use client'
+import Image from "next/image"
+import { useState, useEffect } from "react"
 export default function Home() {
+  const img = [
+    "/image/pic.jpg",
+    "/image/pic2.jpeg",
+    "/image/pic3.jpg"
+  ];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % img.length)
+    }, 3000)
+
+  }, [img.length])
+
   return (
     <main className="bg-white min-h-screen">
+      <div
+        className="w-full h-150 bg-cover bg-center transition-all duration-1000 ease-in-out"
+        style={{
+          backgroundImage: `url(${img[index]})`,
+        }}
+      >
+        {/* Optional content overlay */}
+        <div className="bg-black/40 h-full w-full flex items-center justify-center text-white text-3xl font-bold">
+          Beautiful Bhutan
+        </div>
+      </div>
 
     </main>
   )
