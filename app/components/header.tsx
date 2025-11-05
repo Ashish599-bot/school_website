2f5e4dd93a7bac0c9cba848b26c8fcf40f0ee730
taaro <demal2525@gmail.com>
2025-11-05 14:58:11 +0600

    all

--- app/components/header.tsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="bg-blue-900">
-            <h1 className="text-white text-4xl">T3 Academy</h1>
-
-        </header>
-    )
-}
---
+++ app/extra_curricular/page.tsx
@@ -0,0 +1,8 @@
+export default function Extra() {
+
+    return (
+        <main>
+
+        </main>
+    )
+}
